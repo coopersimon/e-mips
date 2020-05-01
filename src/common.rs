15616a6b73a83c4a8fea995b106e0a4e17f2c0c8
Simon Cooper <simon.d.cooper@hotmail.co.uk>
2020-05-01 13:44:26 +0200

    Modified common to use const fns, added MIPSCore, added branch instructions

--- src/common.rs
+++ src/common.rs
@@ -1,91 +1,83 @@
+// Common helper ops.
 
-// Make a 16-bit value from two 8-bit values.
-macro_rules! make16 {
-    ($lo:expr, $hi:expr) => {
-        ($lo as u16) | (($hi as u16) << 8)
-    };
+// BITWISE
+
+/// Make a value with the selected bit set to 1.
+pub const fn bit(num: usize) -> u32 {
+    1 << num
 }
 
-// Get the low byte of a 16-bit value.
-macro_rules! lo16 {
-    ($val:expr) => {
-        $val as u8
-    };
+/// Test if a bit in `val` is set to 1.
+pub const fn test_bit(val: u32, bit_num: usize) -> bool {
+    let bit = bit(bit_num);
+    (val & bit) != 0
 }
 
-// Get the high byte of a 16-bit value.
-macro_rules! hi16 {
-    ($val:expr) => {
-        ($val >> 8) as u8
-    };
+// BYTEWISE
+
+/// Make a 16-bit value from two 8-bit values.
+pub const fn make16(lo: u8, hi: u8) -> u16 {
+    (lo as u16) | ((hi as u16) << 8)
 }
 
-// Make a 32-bit value from four 8-bit values.
-// The bytes are in order from least significant to most significant.
-macro_rules! make32 {
-    ($b3:expr, $b2:expr, $b1:expr, $b0:expr) => {
-        ($b3 as u32) | (($b2 as u32) << 8) | (($b1 as u32) << 16) | (($b0 as u32) << 24)
-    };
+/// Get the low byte of a 16-bit value.
+pub const fn lo16(val: u16) -> u8 {
+    val as u8
 }
 
-// Get the bytes from a 32-bit value.
-// The bytes are in order from least significant to most significant.
-macro_rules! bytes32 {
-    ($val:expr) => {
-        ($val as u8, ($val >> 8) as u8, ($val >> 16) as u8, ($val >> 24) as u8)
-    };
+/// Get the high byte of a 16-bit value.
+pub const fn hi16(val: u16) -> u8 {
+    (val >> 8) as u8
 }
 
-// Make a 64-bit value from eight 8-bit values.
-macro_rules! make64 {
-    ($b7:expr, $b6:expr, $b5:expr, $b4:expr, $b3:expr, $b2:expr, $b1:expr, $b0:expr) => {
-        ($b7 as u64) | (($b6 as u64) << 8) | (($b5 as u64) << 16) | (($b4 as u64) << 24) |
-        (($b3 as u64) << 32) | (($b2 as u64) << 40) | (($b1 as u64) << 48) | (($b0 as u64) << 56)
-    };
+/// Make a 32-bit value from four 8-bit values.
+/// The bytes are in order from least significant to most significant.
+pub const fn make32(b0: u8, b1: u8, b2: u8, b3: u8) -> u32 {
+    (b0 as u32) | ((b1 as u32) << 8) | ((b2 as u32) << 16) | ((b3 as u32) << 24)
 }
 
-// Get the bytes from a 64-bit value.
-macro_rules! bytes64 {
-    ($val:expr) => {
-        ($val as u8, ($val >> 8) as u8, ($val >> 16) as u8, ($val >> 24) as u8,
-         ($val >> 32) as u8, ($val >> 40) as u8, ($val >> 48) as u8, ($val >> 56) as u8)
-    };
+/// Get the bytes from a 32-bit value.
+/// The bytes are in order from least significant to most significant.
+pub const fn bytes32(val: u32) -> (u8, u8, u8, u8) {
+    (val as u8, (val >> 8) as u8, (val >> 16) as u8, (val >> 24) as u8)
 }
 
-// Get the low word of a 64-bit value.
-macro_rules! lo64 {
-    ($val:expr) => {
-        $val as u32
-    };
+/// Make a 64-bit value from eight 8-bit values.
+pub const fn make64(b0: u8, b1: u8, b2: u8, b3: u8, b4: u8, b5: u8, b6: u8, b7: u8) -> u64 {
+    (b0 as u64) | ((b1 as u64) << 8) | ((b2 as u64) << 16) | ((b3 as u64) << 24) |
+    ((b4 as u64) << 32) | ((b5 as u64) << 40) | ((b6 as u64) << 48) | ((b7 as u64) << 56)
 }
 
-// Get the high word of a 64-bit value.
-macro_rules! hi64 {
-    ($val:expr) => {
-        ($val >> 32) as u32
-    };
+/// Get the bytes from a 64-bit value.
+pub const fn bytes64(val: u64) -> (u8, u8, u8, u8, u8, u8, u8, u8) {
+    (val as u8, (val >> 8) as u8, (val >> 16) as u8, (val >> 24) as u8,
+    (val >> 32) as u8, (val >> 40) as u8, (val >> 48) as u8, (val >> 56) as u8)
 }
 
-// Sign-extend a 8-bit value.
-// Returns a 32-bit unsigned value.
-macro_rules! sign_extend_8 {
-    ($val:expr) => {
-        (($val as i8) as i32) as u32
-    };
+/// Get the low word of a 64-bit value.
+pub const fn lo64(val: u64) -> u32 {
+    val as u32
 }
 
-// Sign-extend a 16-bit value.
-// Returns a 32-bit unsigned value.
-macro_rules! sign_extend_16 {
-    ($val:expr) => {
-        (($val as i16) as i32) as u32
-    };
+/// Get the high word of a 64-bit value.
+pub const fn hi64(val: u64) -> u32 {
+    (val >> 32) as u32
 }
 
-// Sign-extend a 32-bit value.
-// Returns a 64-bit signed value.
-macro_rules! sign_extend_32 {
-    ($val:expr) => {
-        ($val as i32) as i64
-    };
+/// Sign-extend a 8-bit value.
+/// Returns a 32-bit unsigned value.
+pub const fn sign_extend_8(val: u8) -> u32 {
+    ((val as i8) as i32) as u32
+}
+
+/// Sign-extend a 16-bit value.
+/// Returns a 32-bit unsigned value.
+pub const fn sign_extend_16(val: u16) -> u32 {
+    ((val as i16) as i32) as u32
+}
+
+/// Sign-extend a 32-bit value.
+/// Returns a 64-bit signed value.
+pub const fn sign_extend_32(val: u32) -> i64 {
+    (val as i32) as i64
 }
--- src/mem/little.rs
+++ src/mem/little.rs
@@ -8,17 +8,19 @@ macro_rules! impl_mem_16_little {
         impl Mem16 for $struct {
             fn read_halfword(&mut self, addr: Self::Addr) -> u16 {
                 use num_traits::identities::One;
+                use crate::common::*;
 
                 let lo = self.read_byte(addr);
                 let hi = self.read_byte(addr + Self::Addr::one());
-                make16!(lo, hi)
+                make16(lo, hi)
             }
 
             fn write_halfword(&mut self, addr: Self::Addr, data: u16) {
                 use num_traits::identities::One;
+                use crate::common::*;
 
-                let lo = lo16!(data);
-                let hi = hi16!(data);
+                let lo = lo16(data);
+                let hi = hi16(data);
                 self.write_byte(addr, lo);
                 self.write_byte(addr + Self::Addr::one(), hi);
             }
@@ -40,6 +42,7 @@ macro_rules! impl_mem_32_little {
         impl Mem32 for $struct {
             fn read_word(&mut self, addr: Self::Addr) -> u32 {
                 use num_traits::identities::One;
+                use crate::common::*;
 
                 let addr0 = addr;
                 let addr1 = addr0 + Self::Addr::one();
@@ -49,13 +52,14 @@ macro_rules! impl_mem_32_little {
                 let b1 = self.read_byte(addr1);
                 let b2 = self.read_byte(addr2);
                 let b3 = self.read_byte(addr3);
-                make32!(b0, b1, b2, b3)
+                make32(b0, b1, b2, b3)
             }
 
             fn write_word(&mut self, addr: Self::Addr, data: u32) {
                 use num_traits::identities::One;
+                use crate::common::*;
 
-                let bytes = bytes32!(data);
+                let bytes = bytes32(data);
                 let addr0 = addr;
                 let addr1 = addr0 + Self::Addr::one();
                 let addr2 = addr1 + Self::Addr::one();
@@ -79,6 +83,7 @@ macro_rules! impl_mem_64_little {
         impl Mem64 for $struct {
             fn read_doubleword(&mut self, addr: Self::Addr) -> u64 {
                 use num_traits::identities::One;
+                use crate::common::*;
 
                 let addr0 = addr;
                 let addr1 = addr0 + Self::Addr::one();
@@ -96,13 +101,14 @@ macro_rules! impl_mem_64_little {
                 let b5 = self.read_byte(addr5);
                 let b6 = self.read_byte(addr6);
                 let b7 = self.read_byte(addr7);
-                make64!(b0, b1, b2, b3, b4, b5, b6, b7)
+                make64(b0, b1, b2, b3, b4, b5, b6, b7)
             }
 
             fn write_doubleword(&mut self, addr: Self::Addr, data: u64) {
                 use num_traits::identities::One;
+                use crate::common::*;
 
-                let bytes = bytes64!(data);
+                let bytes = bytes64(data);
                 let addr0 = addr;
                 let addr1 = addr0 + Self::Addr::one();
                 let addr2 = addr1 + Self::Addr::one();
